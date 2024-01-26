998eae17c0610245ed3b81b1db256c3b03cf80d1
David Nguyen <h250694@gmail.com>
2024-01-26 21:57:17 +0700

    Update titles and default locale

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -30,8 +30,8 @@ const config = {
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: 'vi',
+    locales: ['vi', 'en'],
   },
 
   presets: [
